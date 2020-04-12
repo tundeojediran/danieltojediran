974207cb67a1e6d0d2bdee8fc9d3b425c307c865
dannytee <tundeojediran@gmail.com>
2020-04-12 17:06:07 +0100

    rename folder

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <header className="">
+          <h1 className="">Daniel 'Tunde' Ojediran</h1>
+          <p>Frontend Engineer | Android Engineer | UI Designer</p>
+        </header>
+      </>
+    );
+  }
+}
+
+export default Header;
